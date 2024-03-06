cbc281aec0a0fa9e428f519a8c760bedef406421
aditya3151 <aditya30997@gmail.com>
2024-03-06 15:46:58 +0530

    cypress/

--- cypress/cypress/e2e/practi.cy.js
+++ cypress/cypress/e2e/practi.cy.js
@@ -1,7 +1,5 @@
 describe("Flipkart", () => {
 
-    let title = null;
-    let price = null
     const url= 'https://docs.cypress.io/api/commands/task#:~:text=Examples%E2%80%8B-,cy.,of%20the%20scope%20of%20Cypress.'
     let syn = null;
 
@@ -22,9 +20,7 @@ describe("Flipkart", () => {
                 cy.log(infoname,infoprice)
             }
         }) 
-        
     })
-
     it.only("locators_2", () => {
         cy.visit(url)
         cy.get('body').trigger('keydown', { keyCode: 27})
@@ -44,21 +40,25 @@ describe("Flipkart", () => {
         })
         cy.get(':nth-child(4) > .transition-all > .overflow-hidden > :nth-child(4) > .group').click()
         cy.get(':nth-child(5) > .codeBlockContent_biex > .prism-code > .codeBlockLines_e6Vv').each(($sort_syn) => {
-
             let sort_syn = $sort_syn.text()
             cy.log(sort_syn)
-        })
-        
+        })  
         cy.get('.theme-doc-markdown > :nth-child(7)').should('be.visible')
         cy.get(':nth-child(57) > a').should('be.visible').click()
         cy.get('.fixed > .border').click()
+        cy.get('body').trigger('keydown', { keyCode: 27})
         cy.log('#Actionability')
         cy.get(':nth-child(10) > :nth-child(1) > a').each(($syn)=>{
              syn = $syn.text()
+             console.log(syn)
             if(syn === 'Scroll the element into view.'){
                 cy.log(syn)
             }
-
         })
-    })
+        cy.go('back')
+        cy.get(':nth-child(2) > .flex').click()
+        cy.get('[href="/api/commands/as"]').should('be.visible').click()
+         })
+
+         
 })
