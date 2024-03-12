35335853d731a3d743f4b74aa1eb82c9a2cc34dc
aditya3151 <aditya30997@gmail.com>
2024-03-12 15:11:08 +0530

    cypress/

--- cypress/cypress/e2e/assessment.cy.js
+++ cypress/cypress/e2e/assessment.cy.js
@@ -4,25 +4,20 @@ describe("Flipkart", () => {
     it("Furniture", () => {
 
         cy.visit("https://www.flipkart.com/")
-        cy.get('._1XjE3T ._27h2j1').eq(2).trigger('mouseover')
-        cy.get('._16rZTH [class="_1BJVlg"]').eq(1).trigger('mouseover')
+        cy.get('[aria-label="Home & Furniture"]').trigger('mouseover')
+        cy.contains('Living Room Furniture').trigger('mouseover')
         cy.contains('Dining Sets').click()
-        cy.get('._2YxCDZ').eq(0).select('10000')
-        cy.get('._2YxCDZ').eq(1).select('30000')
-        cy.get('[title="No Cost EMI"] > ._1Y4Vhm > ._2iDkf8 > ._24_Dny').click()
-        cy.get(':nth-child(5) > ._213eRC > ._2gmUFU').click()
-        cy.get('[title="8 Seater"] > ._1Y4Vhm > ._2iDkf8 > ._30VH1S').check({ force: true })
-        //-----working to print the values--------//
-        cy.get('div[class="_1YokD2 _3Mn1Gg"]').then(($details) => {
-            const details = $details.text()
-            cy.log(details)
-            cy.get('._8VNy32 > ._25b18c > ._30jeq3').each(($el, index, $list) => {
-                const priceString = $el.text()
-                const price = parseFloat(priceString.replace(/[₹,]/g, ''))
-                if (price > 20000) {
-                    cy.log('Final list:' + price)
-                }
-            })
+        cy.contains('Seating Capacity').click()
+        cy.contains('8 Seater').click()
+        //cy.get('[title="No Cost EMI"]').click()
+        cy.get('[data-id]').each($info => {
+            const infoprice = parseFloat($info.find('div[class="_30jeq3"]').text().replace(/[₹,]/g, ''));
+            const infoname = $info.find('[title]').text();
+            // Filter items with price greater than 20000
+            if (infoprice > 20000) {
+                console.log(infoprice); // Log the name of items with price > 20000
+                cy.log(infoname,infoprice)
+            }
         })
     })
 })
